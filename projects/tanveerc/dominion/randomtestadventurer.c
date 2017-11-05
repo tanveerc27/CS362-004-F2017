21d80aeeaa8f4974ae2c690a4629f2c88f7a5b04
Chowdhury TanveerAnzoom <tanveerc@flip1.engr.oregonstate.edu>
2017-11-04 19:28:51 -0700

    Fixed indentation of random test files

--- projects/tanveerc/dominion/randomtestadventurer.c
+++ projects/tanveerc/dominion/randomtestadventurer.c
@@ -1,13 +1,7 @@
-/*
- * adventurer - make sure deck doesn't need to be shuffled
- *		see if card drawn is treasure card, 
- *		increment until 2 treasure cards or deck is empty
- *		discard all drawn cards that aren't treasure cards
- *	- stops when 2 treasure cards are revealed
- * 	- all revealed non-treasure cards are discarded
- *	- no treasure cards in deck (end of deck)
- *	- deck is shuffled when needed (at beginning and middle) **
- */
+/*********************************
+File: randomtestadventurer.c 
+Testing Card: Adventurer
+ *********************************/
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -23,121 +17,115 @@
 #define MAX_RUN 5000
 
 
-int main() {
+int main(){
 
-	int i, j, l;
-    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int seed;
-    int numPlayers = 2;
-    int player = 0;
+   int i, j, l;
+   int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+   int seed;
+   int numPlayers = 2;
+   int player = 0;
 
-	int handTreasureBefore = 0;
-	int handTreasureAfter = 0;
-	int deckTreasureBefore = 0;
-	int deckTreasureAfter = 0;
-	int addTreasures = 0;
-	int handError = 0;
-	int deckError = 0;
+   int handTreasureBefore = 0;
+   int handTreasureAfter = 0;
+   int deckTreasureBefore = 0;
+   int deckTreasureAfter = 0;
+   int addTreasures = 0;
+   int handError = 0;
+   int deckError = 0;
 
-	srand(time(NULL));
+   srand(time(NULL));
 
-	struct gameState G, testG;
-	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-			sea_hag, tribute, smithy, council_room};
+   struct gameState G, testG;
+   int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+      sea_hag, tribute, smithy, council_room};
 
-	
-	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
-	
-	for (i = 0; i < MAX_RUN; i++) 
-	{	
-		seed = (rand() % 2000) + 1;
-		for (l = 0; l < sizeof(struct gameState); l++)
-			((char*)&G)[l] = floor(Random() * 256);
-		
-		// initialize a random game state and random number of cards
-		initializeGame(numPlayers, k, seed, &G);
+   printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
 
-		G.handCount[player] = floor(Random() * MAX_HAND);
-		G.deckCount[player] = floor(Random() * MAX_DECK);
-		G.discardCount[player] = floor(Random() * MAX_DECK);
-		
-		for (j = 0; j < G.handCount[player]; j++) 
-			G.hand[player][j] = floor(Random() * (treasure_map + 1));
-		
-		for (j = 0; j < G.discardCount[player]; j++) 
-			G.discard[player][j] = floor(Random() * (treasure_map + 1));
-		
-		for (j = 0; j < G.deckCount[player]; j++) 
-			G.deck[player][j] = floor(Random() * (treasure_map + 1));
-		
-		// Number of treasure cards in hand before test
-		for(j = 0; j < numHandCards(&G); j++){
-			if (G.hand[player][j] == copper)	handTreasureBefore++;
-			else if (G.hand[player][j] == silver)	handTreasureBefore++;
-			else if (G.hand[player][j] == gold)	handTreasureBefore++;
-		}
+   for (i = 0; i < MAX_RUN; i++){	
+      seed = (rand() % 2000) + 1;
+      for (l = 0; l < sizeof(struct gameState); l++)
+	 ((char*)&G)[l] = floor(Random() * 256);
 
-		// number of treasure cards in deck before test
-		for(j = 0; j < G.deckCount[player]; j++){
-			if (G.deck[player][j] == copper)	deckTreasureBefore++;
-			else if (G.deck[player][j] == silver)	deckTreasureBefore++;
-			else if (G.deck[player][j] == gold)	deckTreasureBefore++;
-		}
+      // initialize a random game state and random number of cards
+      initializeGame(numPlayers, k, seed, &G);
 
-		// number of treasure cards to be added to hand AND to be subtracted from deck
-		if (deckTreasureBefore == 0)	addTreasures = deckTreasureBefore;
-		else if (deckTreasureBefore == 1)	addTreasures = deckTreasureBefore;
-		else if (deckTreasureBefore >= 2)	addTreasures = 2;
+      G.handCount[player] = floor(Random() * MAX_HAND);
+      G.deckCount[player] = floor(Random() * MAX_DECK);
+      G.discardCount[player] = floor(Random() * MAX_DECK);
 
-		// copy the game state to a test case
-		memcpy(&testG, &G, sizeof(struct gameState));
-		cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
-		
-		// number of treasure cards in hand after test
-		for(j = 0; j < numHandCards(&testG); j++){
-			if (testG.hand[player][j] == copper)	handTreasureAfter++;
-			else if (testG.hand[player][j] == silver)	handTreasureAfter++;
-			else if (testG.hand[player][j] == gold)	handTreasureAfter++;
-		}
+      for (j = 0; j < G.handCount[player]; j++) 
+	 G.hand[player][j] = floor(Random() * (treasure_map + 1));
 
-		// number of treasure cards in deck after test
-		for(j = 0; j < testG.deckCount[player]; j++){
-			if (testG.deck[player][j] == copper)	deckTreasureAfter++;
-			else if (testG.deck[player][j] == silver)	deckTreasureAfter++;
-			else if (testG.deck[player][j] == gold)	deckTreasureAfter++;
-		}				
-		
-		/*************** TEST 1: Treasure cards added to hand ***********/
-		//check treasure cards in hand
-		if (handTreasureAfter != handTreasureBefore + addTreasures)
-		{
-			handError++;
-		}
-		
-		/*************** TEST 2: Treasure cards deducted from deck ***********/
-		//check treasure cards in deck
-		if (deckTreasureAfter != deckTreasureBefore - addTreasures)
-		{
-			deckError++;
-		}
+      for (j = 0; j < G.discardCount[player]; j++) 
+	 G.discard[player][j] = floor(Random() * (treasure_map + 1));
 
-		// reset variables
-		handTreasureBefore = 0;
-		handTreasureAfter = 0;
-		deckTreasureBefore = 0;
-		deckTreasureAfter = 0;
-		addTreasures = 0;
-		
-	}
+      for (j = 0; j < G.deckCount[player]; j++) 
+	 G.deck[player][j] = floor(Random() * (treasure_map + 1));
 
-	//print sum of errors for each test type
-	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+      // Number of treasure cards in hand before test
+      for(j = 0; j < numHandCards(&G); j++){
+	 if (G.hand[player][j] == copper)	handTreasureBefore++;
+	 else if (G.hand[player][j] == silver)	handTreasureBefore++;
+	 else if (G.hand[player][j] == gold)	handTreasureBefore++;
+      }
 
-	// High FAIL rate due to the modified code of Adventurer
-	printf("TEST RESULTS\n");
-	printf("Number of Test Runs: %d \n", MAX_RUN);
-	printf("Player hand count failed %d of %d runs\n",handError, MAX_RUN);
-	printf("Player deck count failed %d of %d runs\n\n\n",deckError, MAX_RUN);
+      // number of treasure cards in deck before test
+      for(j = 0; j < G.deckCount[player]; j++){
+	 if (G.deck[player][j] == copper)	deckTreasureBefore++;
+	 else if (G.deck[player][j] == silver)	deckTreasureBefore++;
+	 else if (G.deck[player][j] == gold)	deckTreasureBefore++;
+      }
 
-	return 0;
+      // number of treasure cards to be added to hand AND to be subtracted from deck
+      if (deckTreasureBefore == 0)	addTreasures = deckTreasureBefore;
+      else if (deckTreasureBefore == 1)	addTreasures = deckTreasureBefore;
+      else if (deckTreasureBefore >= 2)	addTreasures = 2;
+
+      // copy the game state to a test case
+      memcpy(&testG, &G, sizeof(struct gameState));
+      cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+
+      // number of treasure cards in hand after test
+      for(j = 0; j < numHandCards(&testG); j++){
+	 if (testG.hand[player][j] == copper)	handTreasureAfter++;
+	 else if (testG.hand[player][j] == silver)	handTreasureAfter++;
+	 else if (testG.hand[player][j] == gold)	handTreasureAfter++;
+      }
+
+      // number of treasure cards in deck after test
+      for(j = 0; j < testG.deckCount[player]; j++){
+	 if (testG.deck[player][j] == copper)	deckTreasureAfter++;
+	 else if (testG.deck[player][j] == silver)	deckTreasureAfter++;
+	 else if (testG.deck[player][j] == gold)	deckTreasureAfter++;
+      }				
+
+      /*************** TEST 1: Treasure cards added to hand ***********/
+      //check treasure cards in hand
+      if (handTreasureAfter != handTreasureBefore + addTreasures)
+	 handError++;
+
+      /*************** TEST 2: Treasure cards deducted from deck ***********/
+      //check treasure cards in deck
+      if (deckTreasureAfter != deckTreasureBefore - addTreasures)
+	 deckError++;
+
+      // reset variables
+      handTreasureBefore = 0;
+      handTreasureAfter = 0;
+      deckTreasureBefore = 0;
+      deckTreasureAfter = 0;
+      addTreasures = 0;
+
+   }
+
+   //print sum of errors for each test type
+   printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+
+   // High FAIL rate due to the modified code of Adventurer
+   printf("TEST RESULTS\n");
+   printf("Number of Test Runs: %d \n", MAX_RUN);
+   printf("Player hand count failed %d of %d runs\n",handError, MAX_RUN);
+   printf("Player deck count failed %d of %d runs\n\n\n",deckError, MAX_RUN);
+
+   return 0;
 }
--- projects/tanveerc/dominion/randomtestcard2.c
+++ projects/tanveerc/dominion/randomtestcard2.c
@@ -1,13 +1,7 @@
-/*
- * adventurer - make sure deck doesn't need to be shuffled
- *		see if card drawn is treasure card, 
- *		increment until 2 treasure cards or deck is empty
- *		discard all drawn cards that aren't treasure cards
- *	- stops when 2 treasure cards are revealed
- * 	- all revealed non-treasure cards are discarded
- *	- no treasure cards in deck (end of deck)
- *	- deck is shuffled when needed (at beginning and middle) **
- */
+/*********************************
+File: randomtestcard2.c 
+Testing Card: Steward
+ *********************************/
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -23,124 +17,123 @@
 #define MAX_RUN 5000
 
 
-int main() {
+int main(){
 
-	int i, j, l;
-    int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-    int seed;
-    int numPlayers = 2;
-    int player = 0;
-    int addCards = 0;
-    int discards = 0;
+   int i, j, l;
+   int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+   int seed;
+   int numPlayers = 2;
+   int player = 0;
+   int addCards = 0;
+   int discards = 0;
 
-	int handCountBefore = 0;
-	int handCountAfter = 0;
-	int deckCountBefore = 0;
-	int deckCountAfter = 0;
-	int coinsBefore = 0;
-	int coinsAfter = 0;
-	int option1error = 0;
-	int option2error = 0;
-	int option3error = 0;
-	int testAddCard = 0;
-	int testCoins = 0;
-	int testTrash = 0;
+   int handCountBefore = 0;
+   int handCountAfter = 0;
+   int deckCountBefore = 0;
+   int deckCountAfter = 0;
+   int coinsBefore = 0;
+   int coinsAfter = 0;
+   int option1error = 0;
+   int option2error = 0;
+   int option3error = 0;
+   int testAddCard = 0;
+   int testCoins = 0;
+   int testTrash = 0;
 
-	srand(time(NULL));
+   srand(time(NULL));
 
-	struct gameState G, testG;
-	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
-			sea_hag, tribute, smithy, council_room};
-
-	
-	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
-	
-	for (i = 0; i < MAX_RUN; i++) 
-	{	
-		seed = (rand() % 2000) + 1;
-		for (l = 0; l < sizeof(struct gameState); l++)
-			((char*)&G)[l] = floor(Random() * 256);
-		
-		// initialize a random game state and random number of cards
-		initializeGame(numPlayers, k, seed, &G);
-
-		G.handCount[player] = floor(Random() * MAX_HAND);
-		G.deckCount[player] = floor(Random() * MAX_DECK);
-		G.discardCount[player] = floor(Random() * MAX_DECK);
-		
-		choice1 = (rand() % 3) + 1;
-		choice2 = floor(Random() * G.handCount[player]);
-		choice2 = floor(Random() * G.handCount[player]);
-		while (choice2 == choice3)
-			choice3 = floor(Random() * G.handCount[player]);
+   struct gameState G, testG;
+   int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+      sea_hag, tribute, smithy, council_room};
 
 
-		for (j = 0; j < G.handCount[player]; j++) 
-			G.hand[player][j] = floor(Random() * (treasure_map + 1));
-		
-		for (j = 0; j < G.discardCount[player]; j++) 
-			G.discard[player][j] = floor(Random() * (treasure_map + 1));
-		
-		for (j = 0; j < G.deckCount[player]; j++) 
-			G.deck[player][j] = floor(Random() * (treasure_map + 1));
-		
-		// hand and deck count before test
-		handCountBefore = G.handCount[player];
-		deckCountBefore = G.deckCount[player];
-		coinsBefore = G.coins;
+   printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
 
-		// copy the game state to a test case
-		memcpy(&testG, &G, sizeof(struct gameState));
-		cardEffect(steward, choice1, choice2, choice3, &testG, handpos, &bonus);
-		
-		// hand and deck count after test
-		handCountAfter = testG.handCount[player];
-		deckCountAfter = testG.deckCount[player];			
-		coinsAfter = testG.coins;
+   for (i = 0; i < MAX_RUN; i++){	
+      seed = (rand() % 2000) + 1;
+      for (l = 0; l < sizeof(struct gameState); l++)
+	 ((char*)&G)[l] = floor(Random() * 256);
 
-		if (choice1 == 1){
-			testAddCard++;
-			addCards = 2;
-			discards = 1;
-			if ((handCountAfter != handCountBefore + addCards - discards) && (deckCountAfter != deckCountBefore - addCards))
-				option1error++;
-		
-		}
+      // initialize a random game state and random number of cards
+      initializeGame(numPlayers, k, seed, &G);
 
-		else if (choice1 == 2) {
-			testCoins++;
-			if (coinsAfter != coinsBefore + 2)
-				option2error++;
+      G.handCount[player] = floor(Random() * MAX_HAND);
+      G.deckCount[player] = floor(Random() * MAX_DECK);
+      G.discardCount[player] = floor(Random() * MAX_DECK);
 
-		}
+      choice1 = (rand() % 3) + 1;
+      choice2 = floor(Random() * G.handCount[player]);
+      choice2 = floor(Random() * G.handCount[player]);
+      while (choice2 == choice3)
+	 choice3 = floor(Random() * G.handCount[player]);
 
-		else if (choice1 == 3) {
-			testTrash++;
-			discards = 3;
-			if ((handCountAfter != handCountBefore - discards) && (deckCountAfter != deckCountBefore))
-				option3error++;
 
-		}
-		
-		// reset variables
-		handCountBefore = 0;
-		handCountAfter = 0;
-		deckCountBefore = 0;
-		deckCountAfter = 0;
-		coinsBefore = 0;
-		coinsAfter = 0;
-		
-	}
+      for (j = 0; j < G.handCount[player]; j++) 
+	 G.hand[player][j] = floor(Random() * (treasure_map + 1));
 
-	//print sum of errors for each test type
-	printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+      for (j = 0; j < G.discardCount[player]; j++) 
+	 G.discard[player][j] = floor(Random() * (treasure_map + 1));
 
-	// High FAIL rate for choice1 = 1, due to the modified code of Steward
-	printf("TEST RESULTS\n");
-	printf("Number of Test Runs: %d \n", MAX_RUN);
-	printf("For Choice1 = 1, Test failed %d of %d runs\n",option1error, testAddCard);
-	printf("For Choice1 = 2, Test failed %d of %d runs\n",option2error, testCoins);
-	printf("For Choice1 = 3, Test failed %d of %d runs\n\n\n",option3error, testTrash);
+      for (j = 0; j < G.deckCount[player]; j++) 
+	 G.deck[player][j] = floor(Random() * (treasure_map + 1));
 
-	return 0;
+      // hand and deck count before test
+      handCountBefore = G.handCount[player];
+      deckCountBefore = G.deckCount[player];
+      coinsBefore = G.coins;
+
+      // copy the game state to a test case
+      memcpy(&testG, &G, sizeof(struct gameState));
+      cardEffect(steward, choice1, choice2, choice3, &testG, handpos, &bonus);
+
+      // hand and deck count after test
+      handCountAfter = testG.handCount[player];
+      deckCountAfter = testG.deckCount[player];			
+      coinsAfter = testG.coins;
+
+      if (choice1 == 1){
+	 testAddCard++;
+	 addCards = 2;
+	 discards = 1;
+	 if ((handCountAfter != handCountBefore + addCards - discards) && (deckCountAfter != deckCountBefore - addCards))
+	    option1error++;
+
+      }
+
+      else if (choice1 == 2) {
+	 testCoins++;
+	 if (coinsAfter != coinsBefore + 2)
+	    option2error++;
+
+      }
+
+      else if (choice1 == 3) {
+	 testTrash++;
+	 discards = 3;
+	 if ((handCountAfter != handCountBefore - discards) && (deckCountAfter != deckCountBefore))
+	    option3error++;
+
+      }
+
+      // reset variables
+      handCountBefore = 0;
+      handCountAfter = 0;
+      deckCountBefore = 0;
+      deckCountAfter = 0;
+      coinsBefore = 0;
+      coinsAfter = 0;
+
+   }
+
+   //print sum of errors for each test type
+   printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+
+   // High FAIL rate for choice1 = 1, due to the modified code of Steward
+   printf("TEST RESULTS\n");
+   printf("Number of Test Runs: %d \n", MAX_RUN);
+   printf("For Choice1 = 1, Test failed %d of %d runs\n",option1error, testAddCard);
+   printf("For Choice1 = 2, Test failed %d of %d runs\n",option2error, testCoins);
+   printf("For Choice1 = 3, Test failed %d of %d runs\n\n\n",option3error, testTrash);
+
+   return 0;
 }
